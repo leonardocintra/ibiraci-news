f865e31c3b6ce1d115810599029009544985c685
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-09 15:39:19 -0200

    Add models

--- artigos/models.py
+++ artigos/models.py
@@ -1,5 +1,31 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
 
 from django.db import models
+from django.db.models import permalink
 
-# Create your models here.
+class Agency(models.Model):
+	name = models.CharField('Nome', max_length=50)
+	site = models.URLField()
+
+	def __str__ (self):
+		return self.name
+
+
+class Author(models.Model):
+	name = models.CharField('Nome', max_length=100)
+	email = models.EmailField()
+
+	def __str__ (self):
+		return self.name
+
+
+class Artigo(models.Model):
+	title = models.CharField('Título', max_length=80)
+	url = models.SlugField('URL', max_length=200, help_text='URL based in title', unique=True)
+	pub_date = models.DateField('Data publicação')	
+	content = models.TextField('Conteudo da página')
+	authors = models.ManyToManyField(Author)
+	agency = models.ForeignKey(Agency)
+
+	def __str__ (self):
+		return self.title
