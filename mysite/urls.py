0463df191aa85a834b66af1b58de0bd31526317d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-02-13 14:04:23 -0200

    add styles and js

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 from artigos.feeds import ArticleRss
 
@@ -7,11 +8,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'artigos.views.index'),
-    url(r'^paginacao/(?P<page>[^\.]+)', 'artigos.views.index'),
+    url(r'^page/(?P<page>[^\.]+)', 'artigos.views.index'),
     url(r'^artigo/(?P<url>[^\.]+)','artigos.views.article'),
     url(r'^form-search/$', 'artigos.views.form_search'),
     url(r'^search/$', 'artigos.views.search'),
     url(r'^contact', 'artigos.views.contact'),
     url(r'^rss/(?P<url>.*)', ArticleRss()),
+    url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
 )
