9cc957b07056ecfa9a02cbca604caebe664722a4
Leonardo Nascimento Cintra <leonardo.cintra@luizalabs.com>
2016-02-15 13:35:07 -0200

    reconfigurando os urls

---
+++ artigos/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.contrib import admin
+from artigos.feeds import ArticleRss
+from . import views
+
+admin.autodiscover()
+
+urlpatterns = [
+	url(r'^$', views.index, name='home'),
+	url(r'^page/(?P<page>[^\.]+)', 'artigos.views.index'),
+    url(r'^artigo/(?P<url>[^\.]+)','artigos.views.article'),
+    url(r'^form-search/$', 'artigos.views.form_search'),
+    url(r'^pesquisa/$', 'artigos.views.search'),
+    url(r'^contato', 'artigos.views.contact'),
+    url(r'^rss/(?P<url>.*)', ArticleRss()),
+    #url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+]
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SITE_ID = 1 # flat_pages
