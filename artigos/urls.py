29020c18656ba8adddfce42d7c1eeeda6cb7debe
Leonardo Nascimento Cintra <leonardo.cintra@luizalabs.com>
2016-02-16 13:24:39 -0200

    convert identations for spaces

--- artigos/urls.py
+++ artigos/urls.py
@@ -6,8 +6,8 @@ from . import views
 admin.autodiscover()
 
 urlpatterns = [
-	url(r'^$', views.index, name='home'),
-	url(r'^page/(?P<page>[^\.]+)', 'artigos.views.index'),
+    url(r'^$', views.index, name='home'),
+    url(r'^page/(?P<page>[^\.]+)', 'artigos.views.index'),
     url(r'^artigo/(?P<url>[^\.]+)','artigos.views.article'),
     url(r'^form-search/$', 'artigos.views.form_search'),
     url(r'^pesquisa/$', 'artigos.views.search'),
