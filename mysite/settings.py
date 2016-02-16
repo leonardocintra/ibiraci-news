beae6d7820a73fbf80fdaf3d9c6ad21ea4fe4d2d
Leonardo Nascimento Cintra <leonardo.cintra@luizalabs.com>
2016-02-16 13:49:12 -0200

    add heroku whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,6 +75,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+# db_from_env = dj_database_url.config(conn_max_age=300)
+DATABASES['default'].update(dj_database_url.config())
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 # Password validation
